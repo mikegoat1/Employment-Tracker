a9f1089dda7cc837b6c010549c0fcd166da0ac7b
Mikey J <mikeylashaun@icloud.com>
2021-06-27 15:55:41 -0700

    updating the scheme

---
+++ index.js
@@ -0,0 +1,134 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: 'Johnson5',
+  database: 'employee_tracker',
+});
+
+const start = () => {
+  inquirer
+    .prompt({
+      name: 'start',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View All employees', 'View departments', 'View Roles','Add employee', "Add department", "Add Role","Update Employee Role"],
+    })
+    .then((answer) => {
+      // based on their answer, either call the bid or the post functions
+      if (answer.start === 'View All employees') {
+        readAllEmployee()
+      } else if (answer.postOrBid === 'BID') {
+        bidAuction();
+      } else {
+        connection.end();
+      }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+const readAllEmployee = () => {
+  console.log('Selecting all employee...\n');
+  connection.query(`SELECT e.id, e.first_name, e.last_name, e.manager_id AS 'manager', role.title, role.salary, department.name AS 'department'
+  FROM employee e
+    LEFT JOIN  role
+      ON (e.role_id = role.id)
+    LEFT JOIN department
+      ON role.department_id = department.id;`, (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table([res]);
+    start(); 
+  });
+};
+
+// const deleteProduct = () => {
+//   console.log('Deleting all strawberry icecream...\n');
+//   connection.query(
+//     'DELETE FROM products WHERE ?',
+//     {
+//       flavor: 'strawberry',
+//     },
+//     (err, res) => {
+//       if (err) throw err;
+//       console.log(`${res.affectedRows} products deleted!\n`);
+//       // Call readProducts AFTER the DELETE completes
+//       readProducts();
+//     }
+//   );
+// };
+
+const updateProduct = () => {
+  console.log('Updating all Rocky Road quantities...\n');
+  const query = connection.query(
+    'UPDATE products SET ? WHERE ?',
+    [
+      {
+        quantity: 100,
+      },
+      {
+        title: 'Rocky Road',
+      },
+    ],
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} products updated!\n`);
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+};
+
+const createProduct = () => {
+  console.log('Inserting a new product...\n');
+  const query = connection.query(
+    'INSERT INTO products SET ?',
+    {
+      flavor: 'Rocky Road',
+      price: 3.0,
+      quantity: 50,
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} product inserted!\n`);
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+};
+
+
+
+// Connect to the DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  start(); 
+});
---
+++ scheme.sql
@@ -0,0 +1,117 @@
+DROP DATABASE IF EXISTS employee_tracker; 
+CREATE database employee_tracker; 
+
+USE employee_tracker; 
+
+CREATE TABLE department ( 
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30), 
+    salary DECIMAL,
+    department_id Int
+); 
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT
+); 
+
+-- This is the select all parameters-- 
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS 'Department'
+FROM employee
+	JOIN (role, department)
+    ON (employee.role_id = role.id AND employee.manager_id = department.id);
+    
+    
+-- This is the correct way of joining rows 
+SELECT e.id, e.first_name, e.last_name, e.manager_id AS 'manager', role.title, role.salary, department.name AS 'department'
+FROM employee e
+	LEFT JOIN  role
+    ON (e.role_id = role.id)
+	LEFT JOIN department
+    ON role.department_id = department.id;
+    
+-- This connects the role and department
+
+SELECT role.title, role.salary, department.name
+from role
+	Join department
+    on role.department_id = department.id;
+
+
+    
+SELECT * FROM department; 
+SELECT * FROM role; 
+SELECT * FROM employee; 
+
+
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("John", "Doe",1,3);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Mike", "Chan",2,1);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Ashley", "Rodriguez",3,null);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Kevin", "Tupik",4,3);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Malia", "Brown",5,null);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Sarah", "Lourd",6,null);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Tom", "Allen",7,6);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id)
+VALUES ("Christian", "Eckenrode",8,2);
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000,1); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000,2); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000,2); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000,3); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000,4); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000,4); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000,2); 
+
+
+INSERT INTO department (name)
+VALUES ("Sales"); 
+
+INSERT INTO department (name)
+VALUES ("Engineering"); 
+
+INSERT INTO department (name)
+VALUES ("Finance"); 
+
+INSERT INTO department (name)
+VALUES ("Legal"); 
+
